7a19b1f4afc30541b6213f9acd7d71623ac51c72
TJBachorz <TJBachorz@gmail.com>
2020-11-11 11:38:33 -0700

    completes accordian menu

--- components/IncidentHeaders.js
+++ components/IncidentHeaders.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import { uniq } from 'lodash';
 import Collapsible from 'react-native-collapsible';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 import Incident from './Incident';
 
@@ -55,6 +55,10 @@ export default function IncidentHeaders({history}) {
         }
     }
 
+    const carrotDownOrRight = (date) => {
+        return !activeArray.includes(date) ? "caretright" : "caretdown"
+    }
+
     return (
         <View style={styles.incidentDateContainer}>
             {incidentDates().map(date => {
@@ -64,12 +68,21 @@ export default function IncidentHeaders({history}) {
                 const day = splitDate[2]
                 return (
                     <View key={date}>
-                        <Text 
-                            style={styles.dateHeader}
-                            onPress={(event) => addOrRemoveFromArray(event, date)}
-                        >
-                            {months[month]} {day}, {year}
-                        </Text>
+                        <View style={styles.iconAndText}> 
+                            <Text style={styles.iconText}>
+                                <Icon
+                                    name={carrotDownOrRight(date)}
+                                    size={15}
+                                    color="#4c96d7"
+                                />
+                            </Text>
+                            <Text 
+                                style={styles.dateHeader}
+                                onPress={(event) => addOrRemoveFromArray(event, date)}
+                            >
+                                    {months[month]} {day}, {year}  
+                            </Text>
+                        </View>
                         <Collapsible 
                             collapsed={!activeArray.includes(date)}
                         >
@@ -88,12 +101,24 @@ export default function IncidentHeaders({history}) {
 
 const styles = StyleSheet.create({
     dateHeader: {
-        margin: 10,
+        padding: 10,
         fontSize: 26,
         color: "#1761a0",
-        fontWeight: "700"
+        fontWeight: "700",
+        paddingRight: 75,
+        // borderWidth: 1,
+        // borderColor: "black",
+        // borderBottomColor: "black",
+        // borderBottomWidth: 1,
+        // borderStyle: "solid"
     },
     incidentDateContainer: {
         margin: 2
+    },
+    iconAndText: {
+        flexDirection: 'row'
+    },
+    iconText: {
+        marginTop: 18
     }
 })
